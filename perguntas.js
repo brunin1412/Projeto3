4ab032bd977ffa989979a88af0823e24d457c5e4
brunin1412 <00001073049139sp@al.educacao.sp.gov.br>
2024-11-27 14:52:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual é a fórmula para calcular a circunferência de um círculo?',
+    'A fórmula para calcular a circunferência é C = 2 * π * r, onde r é o raio.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior oceano do mundo?',
+    'O maior oceano do mundo é o Oceano Pacífico.'
+)
+
+criaCartao(
+    'Física',
+    'O que é energia cinética?',
+    'Energia cinética é a energia que um corpo possui devido ao seu movimento.'
+)
+
+criaCartao(
+    'Literatura',
+    'Qual é o tema principal de "A Moreninha" de Joaquim Manuel de Macedo?',
+    'O tema principal de "A Moreninha" é o amor e suas complexidades, ambientado em uma história romântica e de costumes.'
+)
